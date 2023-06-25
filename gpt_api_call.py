b4c632c15e88c2dd83b53f9d21ed007922ca79e0
Ciaran Maloy <ciaranmaloy@Ciarans-Air.localdomain>
2023-06-25 14:41:57 +0100

    created environment variable

--- gpt_api_call.py
+++ gpt_api_call.py
@@ -1,20 +1,24 @@
 import openai
 import os
+from dotenv import load_dotenv
 
-LOCAL = True
+load_dotenv()
 
-if not LOCAL:
-    try:
-        OPEN_AI_API_KEY = os.environ["OPEN_AI_API_KEY"]
-        openai.api_key = OPEN_AI_API_KEY
-    except KeyError:
-        OPEN_AI_API_KEY = "Token not available!"
-        # or raise an error if it's not available so that the workflow fails
-else:
-    # Set up your OpenAI API credentials
-    with open('.API_KEY/open_ai_api_key.txt') as f:
-        key = f.readlines()[0]
-        openai.api_key = key
+OPEN_AI_API_KEY = os.environ["OPEN_AI_API_KEY"]
+openai.api_key = OPEN_AI_API_KEY
+
+# if not LOCAL:
+#     try:
+#         OPEN_AI_API_KEY = os.environ["OPEN_AI_API_KEY"]
+#         openai.api_key = OPEN_AI_API_KEY
+#     except KeyError:
+#         OPEN_AI_API_KEY = "Token not available!"
+#         # or raise an error if it's not available so that the workflow fails
+# else:
+#     # Set up your OpenAI API credentials
+#     with open('.API_KEY/open_ai_api_key.txt') as f:
+#         key = f.readlines()[0]
+#         openai.api_key = key
 
 
 def ask_gpt(question):
